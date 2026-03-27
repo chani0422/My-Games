74c04b13c68e90963557b562a6f0882eb33e5a36
chani0422 <2102.chani@gmail.com>
2026-03-27 20:39:48 +0900

    feat: build_v2.sh の EXPORTED_FUNCTIONS に _force_shuffle を追加

--- build_v2.sh
+++ build_v2.sh
@@ -29,7 +29,8 @@ echo "=== Building $GAME (v2) -> blackjack.js ==="
     "_free_ptr",
     "_add_funds",
     "_revive",
-    "_debug_deal_ace"
+    "_debug_deal_ace",
+    "_force_shuffle"
   ]' \
   -s EXPORTED_RUNTIME_METHODS='["cwrap","UTF8ToString"]' \
   -o blackjack.js
